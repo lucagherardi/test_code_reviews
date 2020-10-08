65829183ad9cca0952bd7bb94601be652259ca11
Luca Gherardi <lgherardi@veritystudios.com>
2020-10-08 10:39:38 +0200

    Add second line new specification

--- my_file.py
+++ my_file.py
@@ -3,4 +3,4 @@ import os
 
 print(f'Hello {os.getlogin()}')
 print('Welcome to github code reviews with merge feature')
-print('Now we have 2 commits to review urgently')
+print('Now we have 2 commits to review very very urgently')
