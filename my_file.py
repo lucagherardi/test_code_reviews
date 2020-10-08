d26062437a9cbbbeb8ec5bea32e7025e34bfa8ee
Luca Gherardi <lgherardi@veritystudios.com>
2020-10-08 09:59:23 +0200

    Add time

--- my_file.py
+++ my_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import platform
+from datetime import datetime
 
 print(f'Hello {os.getlogin()}')
-print(f'You are using the computer {platform.node()}')
+print(f'You are using the computer {platform.node()}. It is {datetime.now()}')
